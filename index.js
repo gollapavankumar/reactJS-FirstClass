9026fca5dce3c48cb1ccd164bf030874971eac3f
root <gollapavankumar>
2023-03-05 11:04:26 +0000

    reactJsfirstClass

---
+++ index.js
@@ -0,0 +1,7 @@
+const element = (
+  <div>
+    <h1 className="greeting">Hello!</h1>
+    <p className="para"> Nice to see you rahul sir</p>
+  </div>
+);
+ReactDOM.render(element, document.getElementById("root"));
